fa2b17d815858e61400ab1966bf4e7fa9b7d5397
delia <you@example.com>
2023-07-09 18:00:14 -0400

    tag name

--- models/Tag.js
+++ models/Tag.js
@@ -7,7 +7,18 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    // id column
+  id: {
+    type: DataTypes.INTEGER, // Set data type to integer
+    allowNull: false, // Prevent null values
+    primaryKey: true, // Set as primary key
+    autoIncrement: true, // Automatically increment value
   },
+  // tag_name column
+  tag_name: {
+    type: DataTypes.STRING, // Set data type to string
+  },
+},
   {
     sequelize,
     timestamps: false,
