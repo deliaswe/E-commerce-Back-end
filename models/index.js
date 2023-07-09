2b825f9d6998c5d67659f45c62b7425e0f4d9f0b
delia <you@example.com>
2023-07-09 17:45:42 -0400

    import models

--- models/index.js
+++ models/index.js
@@ -4,14 +4,31 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// Define relationships between models
+
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', // Set foreign key in Product model
+});
+
+// A product belongs to tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag, // The intermediary ProductTag model
+  foreignKey: 'product_id', // Set foreign key in ProductTag model
+});
+
+  // A tag belongs to many products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag, // The intermediary ProductTag model
+  foreignKey: 'tag_id', // Set foreign key in ProductTag model
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id', // Set foreign key in Product model
+});
 
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
-
+// Export models
 module.exports = {
   Product,
   Category,
