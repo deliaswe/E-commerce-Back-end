b53f294ae11398f06fff3fae7b37231f911d1aa2
delia <you@example.com>
2023-07-09 17:57:40 -0400

    model and data types

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,29 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    // id column
+  id: {
+    type: DataTypes.INTEGER, // Set data type to integer
+    allowNull: false, // Prevent null values
+    primaryKey: true, // Set as primary key
+    autoIncrement: true, // Automatically increment value
+  },
+  // tag id column
+  tag_id: {
+    type: DataTypes.INTEGER, // Set data type to integer
+    references: { // Create a reference to the tag model's id column
+      model: 'tag',
+      key: 'id',
+    },
+  },
+  // product id column
+  product_id: {
+    type: DataTypes.INTEGER, // Set data type to integer
+    references: { // Create a reference to the product model's id column
+      model: 'product',
+      key: 'id',
+    },
+  },
   },
   {
     sequelize,
