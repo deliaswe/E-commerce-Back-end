28e73eb5a31ea020434a7b179c60fcda3a4c6c05
delia <you@example.com>
2023-07-09 17:36:35 -0400

    define id columns

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
+// Require the dotenv package so we can use the variables we set in the .env file
 require('dotenv').config();
-
+// Require the Sequelize constructor
 const Sequelize = require('sequelize');
-
+// Create a connection to our database, pass in your MySQL information for username and password
+// JAWSDB_URL is an environment variable that Heroku will automatically create and execute for us when we deploy our application.
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
@@ -11,5 +13,5 @@ const sequelize = process.env.JAWSDB_URL
         decimalNumbers: true,
       },
     });
-
+// Export the connection
 module.exports = sequelize;
--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,17 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    // id column
+  id: {
+    type: DataTypes.INTEGER, // Set data type to integer
+    allowNull: false, // Prevent null values
+    primaryKey: true, // Set as primary key
+    autoIncrement: true, // Automatically increment value
+  },
+  // category_name column
+  category_name: {
+    type: DataTypes.STRING, // Set data type to string
+  },
   },
   {
     sequelize,
